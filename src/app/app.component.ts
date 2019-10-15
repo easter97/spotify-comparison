e883fc77e367811b945e19ac525e4e2ebeaea726
Amanda Easter <easter97@tamu.edu>
2019-10-14 23:01:50 -0500

    Routing and basic request skeleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'spotify-comparison';
+  title = 'Spotify Comparison';
+  client_id:string = '06b0d17ee4194381b16f89b55b623ed2'; // Your client id
+  client_secret = '660b85280c684f588a905ea63f4044f8'; // Your secret
+  redirect_uri = 'http://localhost:4200/home'; // Your redirect uri
+  scopes = 'user-read-private user-read-email'
+  ngOnInit(){
+    
+  }
+  
 }
