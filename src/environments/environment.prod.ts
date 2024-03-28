32a3184ee2bb6ffb93cc8d5897b725650603f9c4
Amanda Easter <me@amandaeaster.com>
2024-03-27 22:00:37 -0500

    Remove environments directory from version control

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  redirect_uri: 'http://amandaeaster.com/spotify/home',
-  base_href:"/spotify/",
-  client_id: '06b0d17ee4194381b16f89b55b623ed2', 
-  client_secret: '660b85280c684f588a905ea63f4044f8' 
-};
